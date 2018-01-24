aaee6b9970db51a54cddcdcce453654cf57cb480
Daniel Kreuzhofer <daniel.kreuzhofer@live.com>
2018-01-24 11:02:03 +0100

    add basic software installation for resilio

--- Deployment/customscript.sh
+++ Deployment/customscript.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+# See: https://help.resilio.com/hc/en-us/articles/206178924-Installing-Sync-package-on-Linux
+# on how to install resilio on Linux
+
+# Upgrade to latest packages
+apt update
+apt upgrade -y
+
+# add package source for resilio and install packages
+echo "deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" | sudo tee /etc/apt/sources.list.d/resilio-sync.list
+wget -qO - https://linux-packages.resilio.com/resilio-sync/key.asc | sudo apt-key add -
+apt update
+apt install resilio-sync -y 
+
+# enable autostart for resilio
+systemctl enable resilio-sync
+
